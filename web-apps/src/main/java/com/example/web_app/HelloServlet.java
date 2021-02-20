16e65a82c733aea02389bf3ccdca7046e96ce18f
Your Name <angelicrf@gmail.com>
2021-02-19 19:58:47 -0800

    new web-app-first calc

--- web-apps/src/main/java/com/example/web_app/HelloServlet.java
+++ web-apps/src/main/java/com/example/web_app/HelloServlet.java
@@ -4,22 +4,23 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/calculate")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    /*private String message;
 
     public void init() {
-        message = "Angelique web-app!";
-    }
+        message = "New text";
+    }*/
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+       // response.setContentType("text/html");
 
-        // Hello
+        String getNumOne = request.getParameter("numOne");
+        String getNUmTwo = request.getParameter("numTwo");
+
+        int calcResult = Integer.parseInt(getNumOne) + Integer.parseInt(getNUmTwo);
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        out.println("Num One is " +  getNumOne + " Num two is " + getNUmTwo + " result is " + calcResult);
     }
 
     public void destroy() {
--- web-apps/target/web-app-1.0-SNAPSHOT/index.jsp
+++ web-apps/target/web-app-1.0-SNAPSHOT/index.jsp
@@ -3,15 +3,25 @@
 <html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="index.css">
     <title>Angelique-web-app</title>
 </head>
 <body class="btn-info">
-<h1><%= "Angelique web-app!" %>
-</h1>
-<div class="container btn-primary">
+<div class="container btn-primary mt-4 text-center pt-4 calcMain">
     <h2>Calculator app</h2>
+    <form action="calculate" method="get">
+    <label class="mb-2">
+        <input type="text" name="numOne">
+    </label>
+        <br>
+    <label class="mb-2">
+        <input type="text" name="numTwo">
+    </label>
+        <br>
+        <input type="submit" class="text-center mb-3 btn btn-danger border-dark newBtn" value="Calculate">
+    </form>
+
 </div>
 <br/>
-<a href="hello-servlet">Hello Servlet Start</a>
 </body>
 </html>
