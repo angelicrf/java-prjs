992d2c4237f6d5f5fe6785dc3b031643348f50d1
Your Name <angelicrf@gmail.com>
2021-03-24 14:12:42 -0700

    modified g-login

--- web-apps/src/main/java/com/example/web_app/CalendarClub.java
+++ web-apps/src/main/java/com/example/web_app/CalendarClub.java
@@ -1,10 +1,9 @@
 package com.example.web_app;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.squareup.okhttp.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @WebServlet(name = "calendarclub", value = "/calendarclbs")
@@ -21,15 +22,37 @@ public class CalendarClub extends HttpServlet{
     String storeCode = null;
     String storeAccTkn = null;
     String API_KEY = "AIzaSyCDxl-kP_cWQXry6FO7lpewcZ6OvIVmbkg";
+    List<String> valueSetting = new ArrayList<String>();
+    List<String> idSetting = new ArrayList<String>();
     OkHttpClient client = new OkHttpClient();
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(3000);
+                System.out.println("from golfList ");
+                System.out.println("AccessToken from golfList is " + this.storeAccTkn);
+                getCalendarListGolf(request, this.storeAccTkn, this.API_KEY);
+            } catch (InterruptedException | IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        });
+        try {
+            Thread.sleep(7000);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/calendarclbs.jsp");
+            dispatcher.forward(request, response);
+        } catch (ServletException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
 
         CompletableFuture.runAsync(() -> {
             request.setAttribute("msgData", "Received");
-         });
+        });
 
-        if(request.getParameter("code") != null || request.getRequestURI().contains("code")) {
+        if (request.getParameter("code") != null || request.getRequestURI().contains("code")) {
             CompletableFuture.runAsync(() -> {
                 try {
                     Thread.sleep(3000);
@@ -41,73 +64,76 @@ public class CalendarClub extends HttpServlet{
             CompletableFuture.runAsync(() -> {
                 try {
                     Thread.sleep(3000);
-                    System.out.println("this.storecode is " + this.storeCode);
                     this.storeAccTkn = generateAccessToken(this.storeCode);
+                    System.out.println("accessToken from generateToken is " + this.storeAccTkn);
 
-                } catch (IOException | InterruptedException e) {
+                } catch (IOException | InterruptedException | JSONException e) {
                     e.printStackTrace();
                 }
             });
-        }
-       CompletableFuture.runAsync(() -> {
-            try {
-                Thread.sleep(4000);
-                System.out.println("AccessToken is " + this.storeAccTkn);
-                getCalendarListGolf(this.storeAccTkn , this.API_KEY);
-            } catch (InterruptedException | IOException e) {
+           //old function
+        } //closed if
+        try {
+                Thread.sleep(7000);
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/calendarclbs.jsp");
+                dispatcher.forward(request, response);
+            } catch (ServletException | IOException | InterruptedException e) {
                 e.printStackTrace();
             }
-        });
-        try{
-            Thread.sleep(9000);
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/calendarclbs.jsp");
-            dispatcher.forward(request,response);
-        } catch (ServletException | IOException | InterruptedException e) {
-            e.printStackTrace();
         }
-    }
+
    public String getCodeFromUrl(HttpServletRequest request){
         System.out.println("getCodeFromUrl called");
         this.storeCode = request.getParameter("code");
         return this.storeCode;
    }
-   public String generateAccessToken(String cd) throws IOException {
+   public String generateAccessToken(String cd) throws IOException, JSONException {
         System.out.println("generateAccessToken is called");
 
        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-       RequestBody body = RequestBody.create(mediaType, "grant_type=authorization_code&redirect_uri=http://localhost:8081/web_app_war_exploded/calendarclbs&client_secret=I3lNn3gT6gIbwmanrQtbFyj1&client_id=502305783354-sr48jg1hnse1lehoce3au065rquob8f0.apps.googleusercontent.com&code=" + cd);
+       RequestBody bodyInfo = RequestBody.create(mediaType, "grant_type=authorization_code&redirect_uri=http://localhost:8081/web_app_war_exploded/calendarclbs&client_secret=I3lNn3gT6gIbwmanrQtbFyj1&client_id=502305783354-sr48jg1hnse1lehoce3au065rquob8f0.apps.googleusercontent.com&code=" + cd);
        Request request2 = new Request.Builder()
                .url("https://oauth2.googleapis.com/token")
-               .method("POST", body)
+               .method("POST", bodyInfo)
                .addHeader("Content-Type", "application/x-www-form-urlencoded")
                .build();
+       System.out.println("after build");
+       //create another promise
        Response response2 = this.client.newCall(request2).execute();
-       JsonObject jsonObject = new JsonParser().parse(response2.body().string()).getAsJsonObject();
-       JsonElement jsonElement = jsonObject.get("access_token");
+       String bodyData = response2.body().string();
+       JSONObject jsonObject = new JSONObject(bodyData);
+       String mdAccessTken =  jsonObject.getString("access_token");
+       this.storeAccTkn = mdAccessTken.substring(1, mdAccessTken.length() -1);
+       return  this.storeAccTkn;
+    }
 
-       return jsonElement.toString().substring(1,jsonElement.toString().length() -1);
-   }
-
-    public void getCalendarListGolf(String acTkn, String apiKey) throws IOException {
+    public void getCalendarListGolf(HttpServletRequest request,String acTkn, String apiKey) throws IOException, JSONException {
         System.out.println("getCalList Called " + acTkn + " api key is " + apiKey);
-        Request request2 = new Request.Builder()
+        Request request3 = new Request.Builder()
                 .url("https://www.googleapis.com/calendar/v3/users/me/settings?key=" + apiKey)
                 .method("GET", null)
                 .addHeader("Authorization", acTkn)
                 .addHeader("Content-Type", "applicationx-www-form-urlencoded")
                 .build();
         System.out.println("after request2");
-        Response response2 = this.client.newCall(request2).execute();
-        System.out.println("items are " + response2.body().string());
+        Response response3 = this.client.newCall(request3).execute();
+        String dataObj = response3.body().string();
+        System.out.println("dataObj " + dataObj);
+        JSONObject jsObject = new JSONObject(dataObj);
+        System.out.println("jsObject is " + jsObject);
+        //JSONParser.parse(response2.body().string()).getAsJsonObject();
+        //JsonElement jsonElement = jsonObject.get("items");
+        JSONArray jsArray = jsObject.optJSONArray("items");
 
-        JsonObject jsonObject = new JsonParser().parse(response2.body().string()).getAsJsonObject();
-        JsonElement jsonElement = jsonObject.get("items");
-        JsonArray jsonArray = jsonElement.getAsJsonArray();
-
-       for (int i = 0; i < jsonArray.size(); i++) {
-           System.out.println(i + ". Array element Content:" + jsonArray.get(i));
-           JsonObject jsonObj = jsonArray.get(i).getAsJsonObject();
-           System.out.println("\t value:" + jsonObj.get("value") + " id:" + jsonObj.get("id"));
+       for (int i = 0; i < jsArray.length(); i++) {
+           System.out.println(i + ". Array element Content:" + jsArray.get(i));
+           String value = jsArray.getJSONObject(i).getString("value");
+           String id = jsArray.getJSONObject(i).getString("id");
+           System.out.println("\t value:" + value + " id:" + id);
+           this.valueSetting.add(value);
+           this.idSetting.add(id);
+           request.setAttribute("setId", this.idSetting);
+           request.setAttribute("setValue", this.valueSetting);
        }
     }
 
--- web-apps/src/main/webapp/calendarclbs.jsp
+++ web-apps/src/main/webapp/calendarclbs.jsp
@@ -1,9 +1,10 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
 <html>
 <head>
     <title>Calendar</title>
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="utf-8" http-equiv="content-type">
+    <meta content="utf-8" http-equiv="Content-Type">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="index.css">
@@ -23,7 +24,19 @@
    <% if(getMsg != null){%>
     <a href="https://accounts.google.com/o/oauth2/v2/auth?client_id=502305783354-sr48jg1hnse1lehoce3au065rquob8f0.apps.googleusercontent.com&response_type=code&scope=https://www.googleapis.com/auth/calendar.calendarlist.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar.settings.readonly https://www.googleapis.com/auth/calendar.events.public.readonly&redirect_uri=http://localhost:8081/web_app_war_exploded/calendarclbs&access_type=offline" role="button">GLogin</a>
     <br>
-    <%}%>
+    <form method="post" action="calendarclbs">
+        <input class="btn btn-danger mt-4" type="submit" value="Get AccessToken"/>
+    </form>
+    <br>
+        <% ArrayList<String> getId = (ArrayList<String>)  request.getAttribute("setId");
+           ArrayList<String> getValue = (ArrayList<String>) request.getAttribute("setValue");
+            if(getId != null && getValue != null ){
+               for(int i = 0; i < getId.size(); i ++){%>
+           <ul class="list-group">
+               <li class="list-group-item list-group-item-primary"><%= getId.get(i)%></li>
+               <li class="list-group-item list-group-item-info"><%= getValue.get(i)%></li>
+           </ul>
+        <%}}}%>
 </div>
 </body>
 </html>
